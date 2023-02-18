74b844a1463cb660f00638ed3d87db6a7eb1b597
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-02-18 22:10:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  unsigned long long a = 0;
+  uint64_t a = 0;
   int flag = 0;
   if (value <= 1)
   {
@@ -13,7 +13,7 @@ bool checkPrime(uint64_t value) {
   }
   if (value > 1)
   {
-    for (unsigned long long i = 2; i < value; i++)
+    for (uint64_t i = 2; i < value; i++)
     {
       a = i;
       if (value % a == 0)
@@ -37,9 +37,9 @@ uint64_t nPrime(uint64_t n) {
 // вставьте код функции
   if (n >=1)
   {
-    unsigned long long result = 0;
-    unsigned long long count = 0;
-    for (unsigned long long i = 0; n > count; i++)
+    uint64_t result = 0;
+    uint64_t count = 0;
+    for (uint64_t i = 0; n > count; i++)
     {
       if (checkPrime(i) == true)
       {
@@ -66,8 +66,8 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-  unsigned long long sum = 0;
-  for (unsigned long long i = 0; i < hbound; i++)
+  uint64_t sum = 0;
+  for (uint64_t i = 0; i < hbound; i++)
   {
     if (checkPrime(i) == true)
     {
